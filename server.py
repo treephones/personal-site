380e78ee722e1b75b1985cf60b6ea96ce5e0e49f
Moez B <moezbajwa@hotmail.com>
2020-11-03 22:02:15 -0500

    email form functional

--- server.py
+++ server.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import smtplib
 
 app = Flask(__name__)
+
+def submit_form(name, email, message):
+    form_email = "moezbpersonalsite@gmail.com"
+    pw = "personalsite"
+    me = "moezbajwa@hotmail.com"
+    header = "New Contact Form Submission From MOEZB.COM!"
+    msg = f"{header}\n\nName: {name}\nEmail: {email}\nMessage:\n{message}"
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(form_email, pw)
+    s.sendmail(form_email, me, msg)
+    s.close()
 
 @app.route("/")
 def home():
@@ -14,9 +28,21 @@ def projects():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        message = request.form["msg"]
+        if "@" not in email or "." not in email:
+            return render_template("contact.html", invalid_message="Invalid Email Address!")
+        elif len(message) < 5:
+            return render_template("contact.html", invalid_message="Message not long enough!")
+        else:
+            submit_form(name, email, message)
+            return render_template("sent.html")
+    else:
+        return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run()
