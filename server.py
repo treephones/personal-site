af7ea1facf5a10be7806edecfe97709872356634
Moez B <moezbajwa@hotmail.com>
2020-11-03 20:38:19 -0500

    all pages + contact form shell

--- server.py
+++ server.py
@@ -10,5 +10,13 @@ def home():
 def projects():
      return render_template("projects.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run()
